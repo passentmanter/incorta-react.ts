c9899330e055a1da5735445e45b9b1f1a4bfd54e
passent anter <passent.anter@ascertia.com>
2024-11-10 18:49:13 +0200

    resize image and enhance lazy import

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AppProvider } from "./contexts/AppContext";
 import React, { Suspense } from "react";
 
-// Pages
-// Lazy load pages
-const Home = React.lazy(() => import("./pages/Home"));
-const RaceList = React.lazy(() => import("./pages/Races"));
-const RaceDetails = React.lazy(() => import("./pages/RaceDetails"));
 
 // components
 import Navbar from "./components/navbar";
 
 import "./App.css";
 import Loader from "./components/loader";
+
+// Lazy load pages
+const Home = React.lazy(() => import("./pages/Home"));
+const RaceList = React.lazy(() => import("./pages/Races"));
+const RaceDetails = React.lazy(() => import("./pages/RaceDetails"));
 
 const App: React.FC = () => {
   return (
